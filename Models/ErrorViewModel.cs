b759248b95183fcc80359e85bc8282783e580e66
peanutsch <peanutsch@duck.com>
2025-03-31 09:19:45 +0200

    Creating Github Repository
    Creating Github Repository

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantBestelApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
